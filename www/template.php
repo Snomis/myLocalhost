bcaeededc6da8e4462e3c858a5d0d94ad0f19aa9
Jerry <jerry.simons@gmail.com>
2018-01-10 16:02:21 +0100

    first commit of localhost files 10-01-2018

---
+++ www/template.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Template</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link rel="stylesheet" href="styles.css"> 
+</head>
+<body>
+<h1>HTML5 template pagina</h1>
+
+<p>Hi Jerry.</p>
+</body>
+</html>
